04958d399dc0f7ea23a29070209d2244046b0b26
SirKeka <undergate777@gmail.com>
2025-02-14 15:10:51 +0300

    Update ContainersUtils.java

--- src/Containers/ContainersUtils.java
+++ src/Containers/ContainersUtils.java
@@ -4,31 +4,21 @@ import static java.util.Collections.swap;
 
 public class ContainersUtils {
     public static <T> void selectionSort(T[] array, Comparator<T> comparator) {
-        int minIndex;
-
-        // Поочередно перемещаем границу несортированного подмассива
-        for (int i = 0; i < array.length - 1; i++) {
-            // Найти минимальный элемент в несортированном массиве
-            minIndex = i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (comparator.compare(array[j], array[minIndex]) < 0) {
-                    minIndex = j;
-                }
+        int n = array.length;
+        for (int i = 0; i < n - 1; ++i) {
+            // Находим индекс наименьшего элемента в неотсортированной части массива
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                boolean b = comparator.compare(array[j], array[minIdx]) < 0;
+                // В цикле находим реальный номер ячейки с минимальным значением
+                minIdx = b ? j : minIdx;
             }
-            // Поменять найденный минимальный элемент на первый элемент
-            swap(array[minIndex], array[i]);
+            if (i != minIdx) {
+                T temp = array[i];
+                array[i] = array[minIdx];
+                array[minIdx] = temp;
+            }
         }
-    }
-
-    private static <T> void swap(T t, T t1) {
-        T temp = t1;
-        t1 = t2;
-        t2 = temp;
-    }
-
-
-        public static int mileageCompare(Bus o1, Bus o2) {
-            return Integer.compare(o1.getMileage(), o2.getMileage());
         }
     }
 }
